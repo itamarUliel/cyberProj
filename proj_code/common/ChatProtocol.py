b653e6759c3cf9240322af29399ca172a2972077
itamarUliel <itamar.uliel@gmail.com>
2023-05-05 13:50:50 +0300

    check

--- proj_code/common/ChatProtocol.py
+++ proj_code/common/ChatProtocol.py
@@ -41,6 +41,10 @@ class ChatProtocol:
         return f'{OK_STATUS}|{msg}'
 
     @staticmethod
+    def build_error(msg=""):
+        return f'{ERROR_STATUS}|{msg}'
+
+    @staticmethod
     def build_get_connected_users():
         return f"{CONNECTED_COMMAND}{DELIMITER}"
 
